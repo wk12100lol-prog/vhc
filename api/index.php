3f19a4487cf81fc4bc191a0549ebe6324ae8cb8e
VexHack <dev@vexhack.pl>
2026-07-22 22:46:53 +0200

    fix paths and disable auth

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 // VHC API Router - single entry point for Vercel
-require_once __DIR__ . '/database.php';
-require_once __DIR__ . '/config.php';
+require_once __DIR__ . '/lib/database.inc';
+require_once __DIR__ . '/lib/config.inc';
 
 $input = Database::getJsonInput();
 
